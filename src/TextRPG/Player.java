53b67e1fc7e0e3f38d5a047d6334e8d28afad3bd
Torku7 <saturn9908@gmail.com>
2023-05-07 21:29:33 +0900

    오류 정정

--- src/TextRPG/Player.java
+++ src/TextRPG/Player.java
@@ -2,6 +2,6 @@ package TextRPG;
 //기본 플레이어 hp exp 받고 전투시 hp exp 계산식 받아와 오버라이드
 public class Player {
     static void player(int HP, int Exp){
-        System.out.println(Main.name+"의 정보 : " + HP + "," + Exp);
-}
+
+    }
 }

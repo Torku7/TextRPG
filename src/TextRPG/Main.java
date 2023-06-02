6a4cbba2263c978f339d1c2aee32ce0e2dea7e8e
Yarashii_GURI <dntmdwls42@gmail.com>
2023-06-02 15:37:57 +0900

    try-catch 로 잘못된 입력 시 서비스 종료. 일부 출력문 수정.

--- src/TextRPG/Main.java
+++ src/TextRPG/Main.java
@@ -2,61 +2,70 @@ package TextRPG;
 
 import java.util.Scanner;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 //메인코드입니다
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("어서오세요");
-        System.out.println("게임을 실행하시겠습니까?");
-        System.out.println("1.실행");
-        System.out.println("2.종료");
-        while (true) {
+        Scanner go = new Scanner(System.in);
 
-            Scanner go = new Scanner(System.in);
-            int goend = go.nextInt();
+        try { // Try 문을 통한 잘못된 입력 받을시 종료.
+            System.out.println("TextRPG에 오신 것을 환영합니다.");
+            System.out.println("원하시는 항목을 입력해 주세요.");
+            System.out.println("1.실행");
+            System.out.println("2.종료");
+            System.out.print(">> ");
+            while (true) {
 
-            if (goend == 1) {
-                System.out.println("게임을 실행합니다.");
-                System.out.println("플레이어 이름을 입력해주세요.");
+                int goend = go.nextInt();
 
-                Scanner namesc = new Scanner(System.in);
-                String name = namesc.nextLine();
+                if (goend == 1) {
+                    System.out.println("게임을 실행합니다.");
+                    System.out.print("플레이어 이름을 입력해주세요 : ");
 
-                Player player = new Player(105, 100);//캐릭터 정보 생성[class Player] (시작 HP ex:105/105 경험치 ex:0/100)
+                    Scanner namesc = new Scanner(System.in);
+                    String name = namesc.nextLine();
 
-                System.out.println("게임을 시작합니다");
+                    Player player = new Player(105, 100);//캐릭터 정보 생성[class Player] (시작 HP ex:105/105 경험치 ex:0/100)
+
+                    System.out.println(name + " 님 로그인 되었습니다.");
+                    System.out.println(" ");
+                    System.out.println("게임을 시작합니다");
+                    System.out.println(" ");
 
 
+                    while (true) {
+                        System.out.println("1.전투");
+                        System.out.println("2.휴식");
+                        System.out.println("0.종료");
 
-                while (true) {
-                    System.out.println("1.전투");
-                    System.out.println("2.휴식");
-                    System.out.println("0.종료");
+                        Scanner Play = new Scanner(System.in);
+                        int GamePlay = Play.nextInt();
 
-                    Scanner Play = new Scanner(System.in);
-                    int GamePlay = Play.nextInt();
-
-                    if (GamePlay == 1) {//캐릭터 정보가 전투클래스로 넘어가야함(HP,경험치)
-                       Battle.battle(player);//전투 클래스 호출
-                    } else if (GamePlay == 2) {//캐릭터 정보가 전투클래스로 넘어가야함(HP,경험치)
-                       player.rest();//플레이어의 휴식 호출
-                    } else if (GamePlay == 0) {
-                        System.out.println("게임이 종료되었습니다.");
-                        break;
-                    } else {
-                        System.out.println("잘못 입력하였습니다 숫자 1,2 중에 입력 해주세요.");
+                        if (GamePlay == 1) {//캐릭터 정보가 전투클래스로 넘어가야함(HP,경험치)
+                            Battle.battle(player);//전투 클래스 호출
+                        } else if (GamePlay == 2) {//캐릭터 정보가 전투클래스로 넘어가야함(HP,경험치)
+                            player.rest();//플레이어의 휴식 호출
+                        } else if (GamePlay == 0) {
+                            System.out.println("게임이 종료되었습니다.");
+                            break;
+                        } else {
+                            System.out.println("잘못 입력하였습니다 숫자 1,2 중에 입력 해주세요.");
+                        }
                     }
+                } else if (goend == 2) {
+                    System.out.println("게임이 종료되었습니다.");
+                    break;
+
+                } else {
+                    System.out.println("잘못 입력하였습니다 숫자 1,2 중에 입력 해주세요.");
+
                 }
-            } else if (goend == 2) {
-                System.out.println("게임이 종료되었습니다.");
                 break;
-
-            } else {
-                System.out.println("잘못 입력하였습니다 숫자 1,2 중에 입력 해주세요.");
-
             }
-            break;
+        } catch (Exception e) {
+            System.out.println("잘못된 접근입니다.");
         }
     }
 }

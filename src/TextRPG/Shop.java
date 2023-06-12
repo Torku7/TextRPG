7a510bfdf6cc3a97f1e2fe99c9cc73c465af7527
dntmdwls42 <dntmdwls42@wku.ac.kr>
2023-06-12 11:13:41 +0900

    shop 내 아이템 가격 조정 완료.

--- src/TextRPG/Shop.java
+++ src/TextRPG/Shop.java
@@ -11,9 +11,9 @@ public class Shop {
 
                 System.out.println("상점입니다");
                 System.out.println("보유 코인은 :" + Player.coin);
-                System.out.println("1.포션 : 체력 50회복/코인");
-                System.out.println("2.방어구 : 50데미지를 막아준다/코인");
-                System.out.println("3.무기 : 공격력 5을 올려준다/코인 ");
+                System.out.println("1.포션 : 체력 50회복/100코인");
+                System.out.println("2.방어구 : 50데미지를 막아준다/200코인");
+                System.out.println("3.무기 : 공격력 5을 올려준다/300코인 ");
                 System.out.println("0.상점을 나간다");
 
                 Scanner picks = new Scanner(System.in);
@@ -24,7 +24,7 @@ public class Shop {
                         System.out.println("코인이 부족합니다.");
                     }else {
                         Player.potion++;
-                        Player.coin -= 30;//가격 조정 필요
+                        Player.coin -= 100;
                     }
                 } else if (pick == 2) {
                     if(Player.armor == 1){
@@ -34,7 +34,7 @@ public class Shop {
                             System.out.println("코인이 부족합니다.");
                         }else {
                             Player.shild += 50;
-                            Player.coin -= 30;//가격 조정 필요
+                            Player.coin -= 200;
                             Player.armor++;
                         }
                     }
@@ -46,14 +46,14 @@ public class Shop {
                             System.out.println("코인이 부족합니다.");
                         }else {
                             Player.attack += 5;
-                            Player.coin -= 30;//가격 조정 필요
+                            Player.coin -= 300;
                             Player.weapon++;
                         }
                     }
                 } else if (pick == 0) {
                     return;
                 } else {
-                    System.out.println("잘못입력하셨습니다.");
+                    System.out.println("잘못된 입력입니다.");
                 }
 
             }
